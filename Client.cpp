4a94b77bb3e3b84d8b36128886126e7539753a1d
Ben Dayan <ben.dayan123@gmail.com>
2026-08-30 19:34:14 +0300

    simple boost.asio setup

--- Client.cpp
+++ Client.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
+#define _WIN32_WINNT 0x0A00
+#define BOOST_ASIO_DISABLE_BOOST_COROUTINE
+#define BOOST_ASIO_DISABLE_CO_AWAIT
+#define BOOST_ASIO_DISABLE_STD_COROUTINE
+#define BOOST_ASIO_DISABLE_CONCEPTS
 
-int main()
-{
-    std::cout << "Client is running with C++20" << std::endl;
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main() {
+    try {
+        boost::asio::io_context io_context;
+        std::cout << "Boost.Asio is successfully configured and working!" << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
