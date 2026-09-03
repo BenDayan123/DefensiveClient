a43cea2efdf128cb66ec73050cec3650a4d0f82e
Ben Dayan <ben.dayan123@gmail.com>
2026-09-04 02:16:02 +0300

    Add send and receiveExact methods to NetworkClient for data transmission

--- NetworkClient.cpp
+++ NetworkClient.cpp
@@ -16,6 +16,7 @@ NetworkClient::~NetworkClient() {
     disconnect();
 }
 
+
 bool NetworkClient::connect(const std::string& host, uint16_t port) {
     try {
         std::cout << "[*] Initializing Client I/O context...\n" << std::endl;
@@ -40,6 +41,47 @@ bool NetworkClient::connect(const std::string& host, uint16_t port) {
         return false;
     }
 }
+
+bool NetworkClient::send(const std::vector<uint8_t>& data) {
+    if (!isConnected()) {
+        std::cerr << "[NetworkClient] Error: Attempted to send on a closed socket." << std::endl;
+        return false;
+    }
+    if (data.empty()) 
+        return true;
+
+    try {
+        boost::asio::write(this->socket, boost::asio::buffer(data));
+        std::cout << "[>] Sent '" << data.size() << "' bytes to server." << std::endl;
+        return true;
+    } catch (const boost::system::system_error& e) {
+        return false;
+    }
+}
+
+std::vector<uint8_t> NetworkClient::receiveExact(size_t numberOfBytes) {
+    if (!isConnected() || numberOfBytes == 0) {
+        return {};
+    }
+
+    std::vector<uint8_t> buffer(numberOfBytes);
+    boost::system::error_code error;
+
+    size_t bytes_received = boost::asio::read(this->socket, boost::asio::buffer(buffer), error);
+
+    if (error == boost::asio::error::eof) {
+        std::cout << "[-] Server closed connection." << std::endl;
+    }
+    else if (error) {
+        throw boost::system::system_error(error);
+    }
+    else {
+        std::string response(buffer.begin(), buffer.begin() + bytes_received);
+        std::cout << "[<] Received response (" << bytes_received << " bytes): \"" << response << "\"" << std::endl;
+    }
+    return buffer;
+}
+
 
 bool NetworkClient::isConnected() const {
     return socket.is_open();
